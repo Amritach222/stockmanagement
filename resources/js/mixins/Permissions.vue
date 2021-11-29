d42d19bf3ab80bcc1b662a476a5bd2062cafb8ac
Anusha Khand <anusha.khand@bandipurcablecar.com.np>
2021-11-29 15:58:39 +0545

    Add multiple role permission check function

--- resources/js/mixins/Permissions.vue
+++ resources/js/mixins/Permissions.vue
@@ -10,8 +10,23 @@ export default {
             return status;
         },
         $is(roleName) {
-            let roles = localStorage.getItem('roles')
+            let roles = localStorage.getItem('roles');
             let status = roles.indexOf(roleName) !== -1;
+            return status;
+        },
+        $isAny(rolesName) {
+            let roles = localStorage.getItem('roles');
+            let count = 0;
+            let names = rolesName.split(',');
+            for (let i = 0; i < names.length; i++) {
+                if (roles.indexOf(names[i]) !== -1) {
+                    count = 1;
+                }
+            }
+            let status = false;
+            if (count === 1) {
+                status = true;
+            }
             return status;
         },
     },
