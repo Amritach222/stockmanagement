7f67f56bad11f122cc5a3d84280bc0fd7b140533
Anusha Khand <anusha.khand@bandipurcablecar.com.np>
2022-03-06 16:21:23 +0545

    Add notification dropdown

---
+++ resources/js/components/layouts/TheHeaderDropdownNotify.vue
@@ -0,0 +1,88 @@
+<template>
+    <CDropdown
+        inNav
+        class="c-header-nav-items"
+        placement="bottom-end"
+        add-menu-classes="pt-0"
+    >
+        <template #toggler>
+            <CHeaderNavLink>
+                <div class="c-avatar">
+                    <CIcon name="cil-bell"/>
+<!--                    <img-->
+<!--                        :src=profileImageUrl-->
+<!--                        class="c-avatar-img "-->
+<!--                    />-->
+                </div>
+            </CHeaderNavLink>
+        </template>
+        <CDropdownHeader tag="div" class="text-center" color="light">
+            <strong>Notifications</strong>
+        </CDropdownHeader>
+        <CDropdownItem>
+            <CIcon name="cil-bell"/>
+            Quotations
+            <CBadge color="info" class="mfs-auto">{{ itemsCount }}</CBadge>
+        </CDropdownItem>
+        <CDropdownItem>
+            <CIcon name="cil-bell"/>
+            Purchase Order
+            <CBadge color="success" class="mfs-auto">{{ itemsCount }}</CBadge>
+        </CDropdownItem>
+    </CDropdown>
+</template>
+
+<script>
+import store from "../../store";
+import route from "../../router";
+import ApiServices from "../../services/ApiServices";
+import config from "../../config";
+
+export default {
+    name: 'TheHeaderDropdownNotify',
+    data() {
+        return {
+            itemsCount: 42,
+            profileImageUrl: 'images/avatars/7.jpg',
+            username: 'Username'
+        }
+    },
+    created() {
+        let userData = localStorage.getItem('userData');
+        if(userData !== ''){
+            userData = JSON.parse(userData);
+            this.username = userData.name;
+        }
+        this.profileImage()
+    },
+    methods: {
+        async profileSetting() {
+            route.push({path: '/profile-setting'})
+        },
+        async logoutUser() {
+            store.state.auth.username = '';
+            store.state.auth.password = '';
+            let res = await store.dispatch('auth/logoutUser');
+            if (res) {
+                route.push({path: '/login'})
+            }
+        },
+        async profileImage() {
+            let profilePic = false;
+            if(store.state.auth.isLoggedIn){
+                profilePic = await ApiServices.getUserProfilePic();
+            }
+            if (profilePic.success === true) {
+                let link = profilePic.data;
+                this.profileImageUrl = config.cdnURL + link;
+            }
+        }
+    }
+}
+</script>
+
+<style scoped>
+.c-icon {
+    margin-right: 0.3rem;
+}
+</style>
