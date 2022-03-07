2daebb75878c7232f1195395e0902fbd0f10e226
samundra.adhikari <samundra.adhikari@bandipurcablecar.com.np>
2022-03-07 16:57:29 +0545

    working on purchase order

---
+++ resources/js/components/purchaseOrder/NewPurchaseOrder.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+export default {
+    name: "NewPurchaseOrder"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ resources/js/components/purchaseOrder/PurchaseOrderDetails.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+export default {
+    name: "PurchaseOrderDetails"
+}
+</script>
+
+<style scoped>
+
+</style>
