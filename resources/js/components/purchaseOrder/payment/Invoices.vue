be7fb84b979a0c488955d8628dfb2b4db2c7eed5
Anusha Khand <60502107+anusha-khand4@users.noreply.github.com>
2022-05-02 23:11:36 +0545

    Add print button in invoice

--- resources/js/components/purchaseOrder/payment/Invoices.vue
+++ resources/js/components/purchaseOrder/payment/Invoices.vue
@@ -9,6 +9,11 @@
                             <v-card-title>
                                 Receipt No.: {{ invoiceData.reference_no }}
                                 <v-spacer></v-spacer>
+                                <div class="noprint ml-auto">
+                                    <CButton size="sm" color="success" @click="printInvoice(invoiceData.id)">Print
+                                        Invoice
+                                    </CButton>
+                                </div>
                             </v-card-title>
                             <div class="invoice-container">
                                 <div id="invoice-top">
@@ -79,11 +84,11 @@
                                     <tr class="item" v-for="item in poProducts"
                                         :key='item._id'>
                                         <td class="desc">{{
-                                                item.product.name
+                                            item.product.name
                                             }}
                                         </td>
                                         <td class="id num">{{
-                                                item.received_quantity
+                                            item.received_quantity
                                             }}
                                         </td>
                                         <td class="qty" v-if="item.unit_id">
@@ -95,7 +100,7 @@
                                         <td class="tex">Rs. {{ item.total }}
                                         </td>
                                         <td class="amt">Rs. {{
-                                                item.shipping_cost
+                                            item.shipping_cost
                                             }}
                                         </td>
                                         <td class="amt" v-if="item.tax_id">
@@ -106,7 +111,7 @@
                                         <td class="amt">{{ item.discount }}%
                                         </td>
                                         <td class="amt">Rs. {{
-                                                item.grand_total
+                                            item.grand_total
                                             }}
                                         </td>
                                     </tr>
@@ -148,325 +153,294 @@
     </div>
 </template>
 <script>
-import config from "../../../config";
-import ApiServices from "../../../services/ApiServices";
-import i18n from "../../../i18n";
+    import config from "../../../config";
+    import ApiServices from "../../../services/ApiServices";
+    import i18n from "../../../i18n";
 
-export default {
-    name: "PurchaseOrderBill",
-    data: () => ({
-        cdnURL: config.cdnURL,
-        dialogRegPayment: false,
-        invoice: {
-            amount: '',
-            payment_type: '',
-            description: '',
+    export default {
+        name: "PurchaseOrderBill",
+        data: () => ({
+            cdnURL: config.cdnURL,
+            dialogRegPayment: false,
+            invoice: {
+                amount: '',
+                payment_type: '',
+                description: '',
+            },
+            editedItem: {
+                id: '',
+            },
+            poProducts: [],
+            invoices: [],
+            editPoProducts: [],
+            validated: false,
+            payment: {
+                id: '',
+                reference_no: '',
+                purchase_order_id: '',
+                due_date: '',
+                created_by: '',
+                total: '',
+                tax_id: '',
+                discount: '',
+                grand_total: '',
+                due_amount: '',
+            },
+            vendor: [],
+            settings: {
+                title: '',
+                logo: '',
+                currency: '',
+                date_format: '',
+                fiscal_year_id: '',
+                time_zone: '',
+            },
+        }),
+        async created() {
+            let data = await this.loadData();
+            let settings = await this.getSettings();
+            // this.loadQuoProducts();
         },
-        editedItem: {
-            id: '',
-        },
-        poProducts: [],
-        invoices: [],
-        editPoProducts: [],
-        validated: false,
-        payment: {
-            id: '',
-            reference_no: '',
-            purchase_order_id: '',
-            due_date: '',
-            created_by: '',
-            total: '',
-            tax_id: '',
-            discount: '',
-            grand_total: '',
-            due_amount: '',
-        },
-        vendor: [],
-        settings: {
-            title: '',
-            logo: '',
-            currency: '',
-            date_format: '',
-            fiscal_year_id: '',
-            time_zone: '',
-        },
-    }),
-    async created() {
-        let data = await this.loadData();
-        let settings = await this.getSettings();
-        // this.loadQuoProducts();
-    },
-    methods: {
-        async loadData() {
-            let res = await ApiServices.paymentShow(this.$route.params.id);
-            if (res.success === true) {
-                this.payment = res.data;
-                this.invoices = res.data.invoices;
-                let due = await this.getInvoiceDueAmount();
-                let rtn = await ApiServices.purchaseOrderShow(res.data.purchase_order_id);
-                if (rtn.success === true) {
-                    this.editedItem = rtn.data;
-                    this.poProducts = rtn.data.purchase_order_products;
-                    let ven = await ApiServices.vendorShow(rtn.data.vendor_id);
-                    if (ven.success === true) {
-                        this.vendor = ven.data;
-                    }
-                }
-            }
-        },
-
-        async getInvoiceDueAmount() {
-            var due_amount = this.payment.grand_total;
-            for (var i = 0; i < this.invoices.length; i++) {
-                this.invoices[i].due_amount = due_amount - this.invoices[i].amount;
-                due_amount = this.invoices[i].due_amount;
-            }
-        },
-
-        async getSettings() {
-            let isLoggedIn = localStorage.getItem('isLoggedIn');
-            if (isLoggedIn === 'true') {
-                let res = await ApiServices.settingIndex();
-                if (res.success === true) {
-                    this.settings = res.data;
-                }
-            }
-        },
-
-        async openInvoice() {
-            this.dialogRegPayment = true;
-            this.invoice.amount = this.payment.due_amount;
-            this.invoice.payment_type = 'Cash';
-        },
-
-        async closeRegPayment() {
-            this.dialogRegPayment = false;
-            this.clearData();
-        },
-
-        async createInvoice() {
-            this.validate();
-            if (this.validated === true) {
-                const data = new FormData();
-                data.append('amount', this.invoice.amount);
-                data.append('payment_type', this.invoice.payment_type);
-                data.append('description', this.invoice.description);
-                data.append('payment_id', this.$route.params.id);
-                let res = await ApiServices.invoiceCreate(data);
+        methods: {
+            async loadData() {
+                let res = await ApiServices.paymentShow(this.$route.params.id);
                 if (res.success === true) {
                     this.payment = res.data;
-                    this.dialogRegPayment = false;
-                    this.clearData();
+                    this.invoices = res.data.invoices;
+                    let due = await this.getInvoiceDueAmount();
+                    let rtn = await ApiServices.purchaseOrderShow(res.data.purchase_order_id);
+                    if (rtn.success === true) {
+                        this.editedItem = rtn.data;
+                        this.poProducts = rtn.data.purchase_order_products;
+                        let ven = await ApiServices.vendorShow(rtn.data.vendor_id);
+                        if (ven.success === true) {
+                            this.vendor = ven.data;
+                        }
+                    }
                 }
-            }
-        },
+            },
 
-        async validate() {
-            if (this.invoice.amount > 0) {
-                this.validated = true;
-            } else {
-                this.validated = false;
-            }
-            if (this.invoice.payment_type !== null) {
-                this.validated = true;
-            } else {
-                this.validated = false;
-            }
-        },
+            async getInvoiceDueAmount() {
+                var due_amount = this.payment.grand_total;
+                for (var i = 0; i < this.invoices.length; i++) {
+                    this.invoices[i].due_amount = due_amount - this.invoices[i].amount;
+                    due_amount = this.invoices[i].due_amount;
+                }
+            },
 
-        async clearData() {
-            this.invoice.payment_type = '';
-            this.invoice.amount = '';
-            this.invoice.description = '';
-        },
+            async getSettings() {
+                let isLoggedIn = localStorage.getItem('isLoggedIn');
+                if (isLoggedIn === 'true') {
+                    let res = await ApiServices.settingIndex();
+                    if (res.success === true) {
+                        this.settings = res.data;
+                    }
+                }
+            },
+
+            async printInvoice(id) {
+                var element = document.getElementById("invoice-" + id);
+                element.classList.remove("noprint");
+                for (var i = 0; i < this.invoices.length; i++) {
+                    if (id !== this.invoices[i].id) {
+                        var invoice = document.getElementById("invoice-" + this.invoices[i].id);
+                        invoice.classList.add("noprint");
+                    }
+                }
+                window.print();
+            },
+        }
     }
-}
 </script>
 <style scoped>
 
-.card-btn {
-    color: #fff !important;
-    margin-left: 3px !important;
-}
-
-body {
-    font-family: 'Roboto', sans-serif;
-    font-weight: 400;
-    color: #322d28;
-}
-
-.invoice-container {
-    background: #fff;
-    width: 100%;
-    /* margin: 30px auto; */
-    /* padding: 0.5rem 1.5rem 0 1.5rem; */
-}
-
-[id*='invoice-'] {
-    border-bottom: 1px solid #EEE;
-}
-
-.info {
-    display: block;
-    float: left;
-}
-
-#invoice-top {
-    min-height: 85px;
-    display: flex;
-    align-items: center;
-}
-
-/* #invoice-mid {
-    min-height: 120px;
-} */
-#project {
-    margin-left: 77%;
-}
-
-header.top-bar h1 {
-    font-family: "Montserrat", sans-serif;
-}
-
-.logo {
-    width: 66px;
-}
-
-table {
-    background: #fff;
-    width: 100%;
-    border-collapse: initial !important;
-}
-
-table.invoice .num {
-    font-weight: 200;
-    text-transform: uppercase;
-    letter-spacing: 1.5px;
-    font-size: 0.8em;
-}
-
-table.invoice tr, table.invoice td {
-    background: #fff;
-    text-align: left;
-    font-weight: 400;
-    color: #322d28;
-    width: 345px;
-}
-
-table.invoice tr.header td img {
-    max-width: 300px;
-}
-
-table.invoice tr.header td h2 {
-    text-align: right;
-    font-family: "Montserrat", sans-serif;
-    font-weight: 200;
-    font-size: 2rem;
-    color: #1779ba;
-}
-
-table.invoice tr.intro td:nth-child(2) {
-    text-align: left;
-}
-
-table.invoice tr.details > td {
-    padding-top: 1rem;
-    padding-bottom: 0;
-}
-
-table.invoice tr.details td:last-child, table.invoice tr.details th:last-child {
-    text-align: right;
-}
-
-table.invoice tr.details table thead, table.invoice tr.details table tbody {
-    position: relative;
-}
-
-table.invoice tr.details table thead:after, table.invoice tr.details table tbody:after {
-    content: "";
-    height: 1px;
-    position: absolute;
-    width: 100%;
-    left: 0;
-    margin-top: -1px;
-    background: #c8c3be;
-}
-
-table.invoice tr.totals td {
-    padding-top: 0;
-}
-
-table.invoice tr.totals table tr td {
-    padding-top: 0;
-    padding-bottom: 0.5EM;
-}
-
-table.invoice tr.totals table tr td:nth-child(1) {
-    font-weight: 500;
-}
-
-table.invoice tr.totals table tr td:nth-child(2) {
-    text-align: right;
-    font-weight: 200;
-}
-
-table.invoice tr.totals table tr:nth-last-child(2) td {
-    padding-bottom: 0.5em;
-}
-
-table.invoice tr.totals table tr:nth-last-child(2) td:last-child {
-    position: relative;
-}
-
-table.invoice tr.totals table tr:nth-last-child(2) td:last-child:after {
-    content: "";
-    height: 4px;
-    width: 100%;
-    border-top: 1px solid #1779ba;
-    border-bottom: 1px solid #1779ba;
-    position: relative;
-    right: 0;
-    bottom: -0.575rem;
-    display: block;
-}
-
-table.invoice tr.totals table tr.total td {
-    font-size: 1em;
-    padding-top: 0.5em;
-    font-weight: 700;
-}
-
-table.invoice tr.totals table tr.total td:last-child {
-    font-weight: 700;
-}
-
-.additional-info h5 {
-    font-size: 0.8em;
-    font-weight: 700;
-    text-transform: uppercase;
-    letter-spacing: 2px;
-    color: #1779ba;
-}
-
-h2 {
-    font-size: .9em;
-    margin-bottom: .3em;
-}
-
-p {
-    font-size: .8em;
-    color: #666;
-    line-height: 1em;
-}
-
-span {
-    color: #1413115e;
-}
-
-@media print and (color) {
-    * {
-        -webkit-print-color-adjust: exact;
-        print-color-adjust: exact;
+    .card-btn {
+        color: #fff !important;
+        margin-left: 3px !important;
     }
-}
+
+    body {
+        font-family: 'Roboto', sans-serif;
+        font-weight: 400;
+        color: #322d28;
+    }
+
+    .invoice-container {
+        background: #fff;
+        width: 100%;
+        /* margin: 30px auto; */
+        /* padding: 0.5rem 1.5rem 0 1.5rem; */
+    }
+
+    [id*='invoice-'] {
+        border-bottom: 1px solid #EEE;
+    }
+
+    .info {
+        display: block;
+        float: left;
+    }
+
+    #invoice-top {
+        min-height: 85px;
+        display: flex;
+        align-items: center;
+    }
+
+    /* #invoice-mid {
+        min-height: 120px;
+    } */
+    #project {
+        margin-left: 77%;
+    }
+
+    header.top-bar h1 {
+        font-family: "Montserrat", sans-serif;
+    }
+
+    .logo {
+        width: 66px;
+    }
+
+    table {
+        background: #fff;
+        width: 100%;
+        border-collapse: initial !important;
+    }
+
+    table.invoice .num {
+        font-weight: 200;
+        text-transform: uppercase;
+        letter-spacing: 1.5px;
+        font-size: 0.8em;
+    }
+
+    table.invoice tr, table.invoice td {
+        background: #fff;
+        text-align: left;
+        font-weight: 400;
+        color: #322d28;
+        width: 345px;
+    }
+
+    table.invoice tr.header td img {
+        max-width: 300px;
+    }
+
+    table.invoice tr.header td h2 {
+        text-align: right;
+        font-family: "Montserrat", sans-serif;
+        font-weight: 200;
+        font-size: 2rem;
+        color: #1779ba;
+    }
+
+    table.invoice tr.intro td:nth-child(2) {
+        text-align: left;
+    }
+
+    table.invoice tr.details > td {
+        padding-top: 1rem;
+        padding-bottom: 0;
+    }
+
+    table.invoice tr.details td:last-child, table.invoice tr.details th:last-child {
+        text-align: right;
+    }
+
+    table.invoice tr.details table thead, table.invoice tr.details table tbody {
+        position: relative;
+    }
+
+    table.invoice tr.details table thead:after, table.invoice tr.details table tbody:after {
+        content: "";
+        height: 1px;
+        position: absolute;
+        width: 100%;
+        left: 0;
+        margin-top: -1px;
+        background: #c8c3be;
+    }
+
+    table.invoice tr.totals td {
+        padding-top: 0;
+    }
+
+    table.invoice tr.totals table tr td {
+        padding-top: 0;
+        padding-bottom: 0.5EM;
+    }
+
+    table.invoice tr.totals table tr td:nth-child(1) {
+        font-weight: 500;
+    }
+
+    table.invoice tr.totals table tr td:nth-child(2) {
+        text-align: right;
+        font-weight: 200;
+    }
+
+    table.invoice tr.totals table tr:nth-last-child(2) td {
+        padding-bottom: 0.5em;
+    }
+
+    table.invoice tr.totals table tr:nth-last-child(2) td:last-child {
+        position: relative;
+    }
+
+    table.invoice tr.totals table tr:nth-last-child(2) td:last-child:after {
+        content: "";
+        height: 4px;
+        width: 100%;
+        border-top: 1px solid #1779ba;
+        border-bottom: 1px solid #1779ba;
+        position: relative;
+        right: 0;
+        bottom: -0.575rem;
+        display: block;
+    }
+
+    table.invoice tr.totals table tr.total td {
+        font-size: 1em;
+        padding-top: 0.5em;
+        font-weight: 700;
+    }
+
+    table.invoice tr.totals table tr.total td:last-child {
+        font-weight: 700;
+    }
+
+    .additional-info h5 {
+        font-size: 0.8em;
+        font-weight: 700;
+        text-transform: uppercase;
+        letter-spacing: 2px;
+        color: #1779ba;
+    }
+
+    h2 {
+        font-size: .9em;
+        margin-bottom: .3em;
+    }
+
+    p {
+        font-size: .8em;
+        color: #666;
+        line-height: 1em;
+    }
+
+    span {
+        color: #1413115e;
+    }
+
+    @media print and (color) {
+        * {
+            -webkit-print-color-adjust: exact;
+            print-color-adjust: exact;
+        }
+
+        .noprint {
+            visibility: hidden;
+        }
+    }
 </style>
